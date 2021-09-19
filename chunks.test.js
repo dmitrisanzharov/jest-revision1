6ec679c04ad6e93a10e1fd2086a0998751acb532
Dmitri <dmitrisanzharov@gmail.com>
2021-09-19 08:58:29 +0100

    changed chunks.test.js

--- chunks.test.js
+++ chunks.test.js
@@ -4,7 +4,7 @@ test('test to see if the imported function is successful', () => {
     expect(chunks).toBeDefined();
 });
 
-test('test to see if the imported function is successful', () => {
+test('test to see if chunk functions splits the array based on length correctly', () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const len = 2;
      
